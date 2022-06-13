458ad89cbca5d3f386cf828fa258272661a5b091
tfermoso <tomas.fermoso@gmail.com>
2022-06-13 15:43:49 +0200

    guardando datos en session

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyparser = require("body-parser");
 const Conexion = require("./db");
 session = require('express-session');
 const app = express();
+
 app.use(session({
     secret: '5577-4D44-WppQ38S',
     resave: true,
@@ -15,13 +16,14 @@ app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 
 // Authentication and Authorization Middleware
-var auth = function(req, res, next) {
+var auth = function (req, res, next) {
     console.log("Ejecutando middleware")
     if (req.session && req.session.user)
-      return next();
+        return next();
     else
-      return res.sendStatus(401);
-  };
+        //return res.sendStatus(401);
+        return res.send("/")
+};
 
 
 app.use('/', express.static(__dirname + '/public'));
@@ -33,15 +35,14 @@ app.get('/', function (req, res) {
         data = data.toString().trim().replace("##nombre##", "Login de Página").replace("##err##", "");
         res.send(data);
     })
-
 })
 
 app.post("/", (req, res) => {
     let conexion = new Conexion();
-    let pass= crypto.createHash('md5').update(req.body.password).digest("hex")
+    let pass = crypto.createHash('md5').update(req.body.password).digest("hex")
 
     let consulta = "select * from cliente where username=? and password=?";
-    conexion.con.query(consulta, [req.body.username,pass], (error, results, fields) => {
+    conexion.con.query(consulta, [req.body.username, pass], (error, results, fields) => {
         if (error) {
             fs.readFile("./public/view/login.html", (err, data) => {
                 data = data.toString().trim().replace("##nombre##", "Login de Página").replace("##err##", error.message);
@@ -49,33 +50,23 @@ app.post("/", (req, res) => {
                 return;
             })
         } else {
-            if(results.length>0){
+            if (results.length > 0) {
                 req.session.user = results[0];
-                if (req.body.username == "juan" && req.body.password == "1234") {
-                    fs.readFile("./public/view/contenedor.html", (err, data) => {
-                        data = data.toString().trim();
-                        res.send(data);
-                    })
-                } 
-            }else{
+                fs.readFile("./public/view/contenedor.html", (err, data) => {
+                    data = data.toString().trim();
+                    res.send(data);
+                })
+
+            } else {
                 fs.readFile("./public/view/login.html", (err, data) => {
                     data = data.toString().trim().replace("##nombre##", "Login de Página").replace("##err##", "Usuario o contraseña incorrecto");
                     res.send(data);
                     return;
                 })
-            }        
+            }
         }
+        conexion.con.end();
     });
-
-    conexion.con.end();
-    /*
-    if (req.body.username == "juan" && req.body.password == "1234") {
-        fs.readFile("./public/view/contenedor.html", (err, data) => {
-            data = data.toString().trim();
-            res.send(data);
-        })
-    } 
-    */
 })
 
 app.post('/login', function (req, res) {
@@ -86,12 +77,12 @@ app.post('/login', function (req, res) {
 app.get('/logout', function (req, res) {
     req.session.destroy();
     res.send("logout success!");
-  });
-  
-  // Get content endpoint
-  app.get('/content', auth, function (req, res) {
-      res.send("You can only see this after you've logged in.");
-  });
-  
+});
+
+// Get content endpoint
+app.get('/content', auth, function (req, res) {
+    res.send("You can only see this after you've logged in.");
+});
+
 
 app.listen(3000)
